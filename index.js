edfbdf880d53e2953aeb01caaa50076c2f3c9e9d
RNCAT <jaruwat.works@gmail.com>
2021-06-20 07:00:32 +0700

    :heavy_plus_sign: add compression

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const compression = require('compression')
+
 const { User, Raft, Boat, Driver } = require('./routes')
 const { ENV } = require('./config')
 
 const app = express()
 
 app.use(cors())
+app.use(compression())
 app.use(express.json())
+
 app.use('/user', User)
 app.use('/driver', Driver)
 app.use('/boat', Boat)
