08b3949502855cfdb4b4758763272071761dc479
RNCAT <jaruwat.works@gmail.com>
2021-06-20 07:40:06 +0700

    :art: add startApp function

--- index.js
+++ index.js
@@ -6,17 +6,29 @@ const compression = require('compression')
 
 const { User, Raft, Boat, Driver } = require('./routes')
 const { ENV } = require('./config')
+const { syncAll } = require('./models')
 
-const app = express()
+async function startApp() {
+    const app = express()
 
-app.use(cors())
-app.use(compression())
-app.use(helmet())
-app.use(express.json())
+    app.use(cors())
+    app.use(compression())
+    app.use(helmet())
+    app.use(express.json())
 
-app.use('/user', User)
-app.use('/driver', Driver)
-app.use('/boat', Boat)
-app.use('/raft', Raft)
+    app.use('/user', User)
+    app.use('/driver', Driver)
+    app.use('/boat', Boat)
+    app.use('/raft', Raft)
 
-app.listen(ENV.NODE_PORT)
+    await syncAll(true)
+
+    try {
+        app.listen(ENV.NODE_PORT)
+        console.log(`Server is running on http://localhost:${ENV.NODE_PORT}`)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+startApp()
