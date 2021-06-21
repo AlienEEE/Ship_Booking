ab803485c5685f83dbdd75eda8ba8d76b90d1aca
thanyaratp <624234009@parichat.skru.ac.th>
2021-06-21 17:28:04 +0700

    :sparkles: add syncOneToMany function

--- models/database.js
+++ models/database.js
@@ -13,5 +13,16 @@ async function syncAll(force = false) {
 async function syncOne(model, force = false) {
     await model.sync({ force: force })
 }
+async function syncOneToMany(one, many, fk) {
+    await one.hasMany(many, {
+        foreignKey: {
+            name: fk,
+            field: fk,
+            allowNull: false,
+        },
+        onDelete: 'RESTRICT',
+        onUpdate: 'CASCADE',
+    })
+}
 
-module.exports = { sequelize, syncAll, syncOne }
+module.exports = { sequelize, syncAll, syncOne, syncOneToMany }
