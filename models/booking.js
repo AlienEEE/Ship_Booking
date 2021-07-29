005b042e9034f86b25e6a96eab8e280ec6088bf3
thanyaratp <624234009@parichat.skru.ac.th>
2021-07-29 20:44:28 +0700

    :art: update

--- models/booking.js
+++ models/booking.js
@@ -1,45 +1,51 @@
 const { sequelize } = require('./database')
 const { DataTypes } = require('sequelize')
 
-const Booking = sequelize.define('Booking', {
-  value: {
-    field: 'booking_value',
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  price: {
-    field: 'booking_price',
-    type: DataTypes.FLOAT,
-    allowNull: false
-  },
-  booking_date: {
-    field: 'booking_date',
-    type: DataTypes.DATE,
-    allowNull: false
-  },
-  travel_date: {
-    field: 'travel_date',
-    type: DataTypes.DATE,
-    allowNull: false
-  },
-  payment: {
-    field: 'booking_payment',
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  status: {
-    field: 'booking_status',
-    type: DataTypes.STRING(15),
-    allowNull: false
-  },
-  package_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  user_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  }
-})
+const Booking = sequelize.define(
+    'Booking',
+    {
+        value: {
+            field: 'booking_value',
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        price: {
+            field: 'booking_price',
+            type: DataTypes.FLOAT,
+            allowNull: false
+        },
+        booking_date: {
+            field: 'booking_date',
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        travel_date: {
+            field: 'travel_date',
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        payment: {
+            field: 'booking_payment',
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        status: {
+            field: 'booking_status',
+            type: DataTypes.STRING(15),
+            allowNull: false
+        },
+        package_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    },
+    {
+        tableName: 'bookings'
+    }
+)
 
 module.exports = Booking
